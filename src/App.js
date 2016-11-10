e279b2c87a7ff1f5fe30bb6f4790aea936637eba
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2016-11-10 13:33:20 -0500

    console.err instead of log, small styling

--- src/App.js
+++ src/App.js
@@ -26,14 +26,14 @@ class App extends Component {
       this.setState({celebrities: response.data})
     })
     .catch(err => {
-      console.log(err)
+      console.error(err)
     })
   }
 
   renderCelebs () {
     const { celebrities } = this.state
     if (!celebrities) {
-      return <div>Loading...</div>
+      return <h2>Loading...</h2>
     }
     return celebrities.map((celeb, i) =>
       <Celebrity
@@ -52,8 +52,8 @@ class App extends Component {
         </div>
         <div className='App-intro'>
           <p>This site is using the <a href='https://dev.twitter.com/docs'>Twitter</a> and <a href='https://developers.facebook.com/docs/graph-api'>Facebook</a> APIs to load information for ten celebrities.</p>
-          <p>Source code for the front end is located <a href='https://github.com/gp3gp3gp3/fb-twitter-celebrities-list'>here</a>.</p>
-          <p>Source code for the back end is located <a href='https://github.com/gp3gp3gp3/Sinatra-Oauth-backend'>here</a>.</p>
+          <p>Source code for the front end is located <a href='https://github.com/gp3gp3gp3/fb-twitter-celebrities-list'>here</a></p>
+          <p>Source code for the back end is located <a href='https://github.com/gp3gp3gp3/Sinatra-Oauth-backend'>here</a></p>
           {this.renderCelebs()}
         </div>
       </div>
